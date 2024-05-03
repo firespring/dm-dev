7ab1a2f44a57d74de97db0b0d8bab1b9c17a1789
Daryn Warriner <daryn.warriner@firespring.com>
2024-05-03 12:39:42 -0500

    [story/TP-95093]: Add pull command to setup script and update checkout so it outputs a warning if the working directory is dirty

--- setup.rb
+++ setup.rb
@@ -39,17 +39,22 @@ module DatamapperProject
     def checkout(branch_name, create: false)
       Dir.chdir(config.dev_root) do
         config.include_repos.each do |repo|
-          @included_repos_actions.call(repo) do |_config, _github_repo, dest_path, is_repo|
+          @included_repos_actions.call(repo) do |_config, github_repo, dest_path, is_repo|
             next unless is_repo
 
             Dir.chdir(dest_path) do
+              unless clean?(dest_path)
+                puts "#{github_repo} working directory is dirty. Unable to checkout #{branch_name}"
+                next
+              end
+
               if create
                 # @todo this probably needs some error handling or checking to see if the branch already exists
                 puts "Create and Checkout branch: #{branch_name}"
                 `git checkout -b #{branch_name}; git push -u origin`
               else
                 puts "Checkout branch: #{branch_name}"
-                `git checkout #{branch_name}`
+                `git pull; git checkout #{branch_name}`
               end
             end
           end
@@ -77,6 +82,35 @@ module DatamapperProject
 
       `$(cd #{path}; git rev-parse --is-inside-work-tree 2>/dev/null)`
     end
+
+    def clean?(path)
+      `cd #{path}; git status --porcelain`.empty?
+    end
+
+    def current_branch(path)
+      `cd #{path}; git rev-parse --abbrev-ref HEAD`.strip
+    end
+
+    def pull_included_repos
+      Dir.chdir(config.dev_root) do
+        config.include_repos.each do |repo|
+          @included_repos_actions.call(repo) do |_config, github_repo, dest_path, is_repo|
+            next unless is_repo
+
+            Dir.chdir(dest_path) do
+              branch = current_branch(dest_path)
+              if clean?(dest_path)
+                puts "Pull branch: #{branch} for #{github_repo}"
+                `git pull`
+              else
+                puts "Branch #{branch} is dirty. Unable to pull for #{github_repo}"
+                next
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
 
@@ -85,6 +119,7 @@ def main(_args)
     opt :clone, 'Clone all datamapper included repos into DM_DEV_ROOT'
     opt :checkout, 'Checkout specified branch for all included repos', type: String, default: 'master'
     opt :create, "Create the checked out branch if it doesn't exist"
+    opt :pull, 'Pull specified branch'
   end
 
   config = DatamapperProject::Config.new
@@ -98,6 +133,8 @@ def main(_args)
       git_commands.clone_included_repos
     in checkout_given: true
       git_commands.checkout(opts[:checkout])
+    in pull: true
+      git_commands.pull_included_repos
     end
   end
 end
