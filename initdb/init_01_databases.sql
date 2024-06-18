ea4a16d2bf6ff5cde912e8bd7261056e73f0f44d
Daryn L Warriner <daryn.warriner@firespring.com>
2024-06-18 17:05:37 -0500

    [story/TP-95094]: Roll mysql image back to 5.7 version to try to minimize syntax errors while we fix other things. Once we've got tests passing we can worry about upgrading to mysql8 Add missing test database and permissions

--- initdb/init_01_databases.sql
+++ initdb/init_01_databases.sql
@@ -4,6 +4,10 @@ CREATE DATABASE datamapper_default_tests;
 DROP DATABASE IF EXISTS datamapper_alternate_tests;
 CREATE DATABASE datamapper_alternate_tests;
 
+DROP DATABASE IF EXISTS do_test;
+CREATE DATABASE do_test;
+
 CREATE USER 'datamapper'@'%' IDENTIFIED BY 'datamapper';
 GRANT ALL ON datamapper_default_tests.* TO 'datamapper'@'%';
 GRANT ALL ON datamapper_alternate_tests.* TO 'datamapper'@'%';
+GRANT ALL ON do_test.* TO 'datamapper'@'%';
